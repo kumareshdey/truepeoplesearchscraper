fa733fc9b882cfbc2965d2ea31ca7bd4e1a868e2
Kumaresh Dey <kumareshdey@Kumareshs-Laptop.local>
2024-06-06 00:03:00 +0530

    new ui

---
+++ scraper.py
@@ -0,0 +1,253 @@
+import urllib.parse
+from bs4 import BeautifulSoup
+from fuzzywuzzy import fuzz
+import requests
+from selenium import webdriver
+import logging
+import warnings
+import time
+from logging import config
+from contextlib import contextmanager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import openpyxl
+import pandas as pd
+import os
+
+from credentials import SCRAPEOPS_CREDS
+
+@contextmanager
+def get_driver():
+    chrome_options = Options()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration (useful for headless mode)
+    chrome_options.add_argument("--no-sandbox")  # Bypass OS security model (useful for Docker)
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
+    driver = webdriver.Chrome(options=chrome_options)
+    try:
+        yield driver
+    finally:
+        driver.quit()
+
+
+def retry(max_retry_count, interval_sec):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            log = args[0].log 
+            retry_count = 0
+            while retry_count < max_retry_count:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    retry_count += 1
+                    log.error(f'{func.__name__} failed on attempt {retry_count}: {str(e)}')
+                    if retry_count < max_retry_count:
+                        log.info(f'Retrying {func.__name__} in {interval_sec} seconds...')
+                        time.sleep(interval_sec)
+            log.warning(f'{func.__name__} reached maximum retry count of {max_retry_count}.')
+            raise Exception(e)
+        return wrapper
+    return decorator
+
+
+class Usps:
+    def __init__(self, log: logging, zip) -> None:
+        self.log = log
+        self.zip = zip
+
+
+    def unique_city(self, city_list):
+        unique_cities = []
+        seen_prefixes = set()
+
+        for city in city_list:
+            prefix = city[:3]
+            if prefix not in seen_prefixes:
+                unique_cities.append(city)
+                seen_prefixes.add(prefix)
+        
+        return unique_cities
+
+
+    @retry(max_retry_count=3, interval_sec=5)
+    def get_city_from_zipcode(self):
+        self.log.info(f"Fetching city of zipcode = {self.zip}")
+        with get_driver() as driver:
+            driver.get("https://tools.usps.com/zip-code-lookup.htm?citybyzipcode")
+            zip_field = driver.find_element(By.ID, "tZip")
+            zip_field.send_keys(self.zip)
+            submit = driver.find_element(By.ID, """cities-by-zip-code""")
+            submit.click()
+            wait = WebDriverWait(driver, 20)
+            wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "recommended-cities")))
+            soup = BeautifulSoup(driver.page_source, "lxml")
+            recommended = [text.text for text in soup.find(class_="recommended-cities").find_all(class_="row-detail-wrapper")]
+            others = [text.text for text in soup.find(class_="other-city-names").find_all(class_="row-detail-wrapper")]
+            recommended.extend(others)
+            recommended = self.unique_city(recommended)
+            self.log.info(f"Found cities: {recommended}")
+            return recommended
+        
+
+class Truepeoplesearch:
+    def __init__(self, log: logging, first_name='', last_name='', street='', city='', dist='', zip='') -> None:
+        self.log = log
+        self.first_name = first_name
+        self.last_name = last_name
+        self.street = street
+        self.city = city
+        self.dist = dist
+        self.zip = zip
+        self.BASE_URL = "https://www.truepeoplesearch.com"
+
+    def proxied_request(self, url):
+        PROXY_URL = 'https://proxy.scrapeops.io/v1/'
+        API_KEY = SCRAPEOPS_CREDS
+        return requests.get(
+            url=PROXY_URL,
+            params={
+                'api_key': API_KEY,
+                'url': url, 
+                # 'residential': 'true', 
+                'country': 'us'
+            },
+        )
+
+    @retry(max_retry_count=3, interval_sec=5)
+    def get_pople_search_result(self, name, address):
+        base_url = f"{self.BASE_URL}/results?"
+        # Encode the name and address for use in a URL
+        encoded_name = urllib.parse.quote(name)
+        encoded_address = urllib.parse.quote(address)
+        # Construct the full URL
+        full_url = f"{base_url}name={encoded_name}&citystatezip={encoded_address}"
+        self.log.info(f"Url: {full_url}")
+        response = self.proxied_request(full_url)
+        if response.status_code != 200:
+            raise Exception(f"Status_code: {response.status_code}, Text: {response.text}")
+        return response.text
+        
+    def get_links_of_all_results(self, result):
+        soup = BeautifulSoup(result, 'html.parser')
+        names = soup.find_all('div', class_='card-summary')
+        self.log.info(f"Got {len(names)} entries for the search")
+        return [self.BASE_URL+name.get("data-detail-link") for name in names]
+
+    def get_emails(self, soup: BeautifulSoup):
+        emails = []
+        slots = soup.find_all(class_='row pl-md-1')
+        for slot in slots:
+            if "Email Addresses" in slot.get_text():
+                emails= [email.text.strip() for email in slot.find_all(class_="col")]
+                break
+        allowed_domains = [
+            "@yahoo.com",
+            "@hotmail.com",
+            "@gmail.com",
+            "@aol.com",
+            "@msn.com",
+            "@outlook.com",
+            "@live.com"
+        ]
+        return [email for email in emails if any(domain in email for domain in allowed_domains)]
+    
+    def compare_addresses(self, address1, address2):
+        similarity_score = fuzz.partial_ratio(address1.lower(), address2.lower())
+        self.log.info(f"Matched addesses ({address1} | AND | {address2}) and got similarity score of {similarity_score}")
+        return similarity_score >=90
+
+    @retry(max_retry_count=3, interval_sec=5)
+    def get_emails_after_verifying_address(self, url, source_address):
+        response = self.proxied_request(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        addresses = soup.find_all(lambda tag: tag.get('data-link-to-more') == 'address')
+        for address in addresses:
+            address = address.find_all('span')
+            address = " ".join([add.text for add in address])
+            if self.compare_addresses(address, source_address):
+                emails = self.get_emails(soup)
+                return emails
+        return None
+
+    def truepeoplesearch_manager(self, name, address):
+        result = self.get_pople_search_result(name, address)
+        links = self.get_links_of_all_results(result)
+        for link in links:
+            emails = self.get_emails_after_verifying_address(link, address)
+            if emails:
+                self.log.info(f"Got emails {emails}")
+                return emails
+        self.log.error("Got no emails.")
+        return []
+
+def process_row(row, result_excel_file_path, log: logging):
+    try:
+        log.info(f"Scraping for: {row}")
+        usps = Usps(zip=row["ZIP"], log=log)
+        cities = usps.get_city_from_zipcode()
+        rows = []
+        for city in cities:
+            city = city.split(" ")
+            city, dist = ' '.join(city[:-1]), city[-1]
+            truepeoplesearch = Truepeoplesearch(
+                first_name=row["FIRST_NAME"],
+                last_name=row["LAST_NAME"],
+                street=row["STREET"],
+                city=city,
+                dist=dist,
+                zip=str(row["ZIP"]),
+                log=log
+            )
+            emails = truepeoplesearch.truepeoplesearch_manager(
+                name=" ".join([row["FIRST_NAME"], row["LAST_NAME"]]), 
+                address=" ".join([city, dist, str(row["ZIP"])]))
+            if not emails:
+                emails = truepeoplesearch.truepeoplesearch_manager(
+                name=" ".join([row["FIRST_NAME"].split(" ")[0], row["LAST_NAME"]]), 
+                address=" ".join([city, dist, str(row["ZIP"])]))
+            new_row = {
+            "FIRST_NAME": row["FIRST_NAME"],
+            "LAST_NAME": row["LAST_NAME"],
+            "STREET": row["STREET"],
+            "CITY": city,
+            "DIST": dist,
+            "ZIP": row["ZIP"],
+            "EMAIL": emails,
+            "STATUS": 'SUCCESS'
+        }
+            rows.append(new_row)
+    except:
+        new_row = {
+            "FIRST_NAME": row["FIRST_NAME"],
+            "LAST_NAME": row["LAST_NAME"],
+            "STREET": row["STREET"],
+            "CITY": city,
+            "DIST": dist,
+            "ZIP": row["ZIP"],
+            "EMAIL": [],
+            "STATUS": "ERROR"
+        }
+        rows.append(new_row)
+    df = pd.DataFrame(rows)
+    if os.path.exists(result_excel_file_path):
+        existing_df = pd.read_excel(
+            result_excel_file_path, names=["FIRST_NAME", "LAST_NAME", "STREET", "CITY", "DIST", "ZIP", "EMAIL", "STATUS"], engine="openpyxl"
+        )
+        existing_df = pd.concat([existing_df, df], ignore_index=True)
+        df = existing_df
+    else:
+        with open(result_excel_file_path, "w"):
+            pass
+
+    df = df.explode("EMAIL", ignore_index=True)
+    duplicated_rows = df.duplicated(subset=["FIRST_NAME", "LAST_NAME", "STREET", "CITY", "DIST", "ZIP", "STATUS"])
+    df.loc[duplicated_rows, ["FIRST_NAME", "LAST_NAME", "STREET", "CITY", "DIST", "ZIP", "STATUS"]] = ""
+    log.info(f"Saved to excel: {result_excel_file_path}")
+
+    return df
---
+++ ui.py
@@ -0,0 +1,181 @@
+import threading
+import openpyxl
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
+import traceback
+import pandas as pd
+import logging
+from scraper import process_row
+
+
+class Logger(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.textbox = tk.Text(self, width=130, height=50, state="disabled")
+        self.scrollbar = tk.Scrollbar(self, orient="vertical", command=self.textbox.yview)
+        self.textbox.config(yscrollcommand=self.scrollbar.set)
+        self.textbox.tag_config("info")
+        self.textbox.tag_config("error", foreground="red")
+        self.textbox.pack(side="left", fill="y")
+        self.scrollbar.pack(side="right", fill="y")
+
+    def log_text(self, text: str, tag: str) -> None:
+        self.textbox.config(state="normal")
+        self.textbox.insert("end", f"{text}\n", tag)
+        self.textbox.config(state="disabled")
+        self.textbox.see(tk.END)
+
+    def info(self, text: str) -> None:
+        self.log_text(text, "info")
+
+    def error(self, text: str) -> None:
+        self.log_text(text, "error")
+
+
+class TextHandler(logging.Handler):
+    """A custom logging handler that sends log messages to a Tkinter Text widget."""
+
+    def __init__(self, logger_widget):
+        super().__init__()
+        self.logger_widget = logger_widget
+
+    def emit(self, record):
+        msg = self.format(record)
+        tag = "info" if record.levelno < logging.ERROR else "error"
+        self.logger_widget.log_text(msg, tag)
+
+
+class ExcelProcessorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Truepeoplesearch scraper")
+
+        # Title
+        self.title_label = tk.Label(root, text="Truepeoplesearch scraper", font=("Arial", 24))
+        self.title_label.pack(pady=10)
+
+        # Input for source Excel file
+        self.source_label = tk.Label(root, text="Select Source Excel File:")
+        self.source_label.pack(pady=5)
+        self.source_entry = tk.Entry(root, width=60)
+        self.source_entry.pack(pady=5)
+        self.source_button = tk.Button(root, text="Browse", command=self.browse_source_file)
+        self.source_button.pack(pady=5)
+
+        # Input for destination Excel file
+        self.dest_label = tk.Label(root, text="Select Destination Excel File:")
+        self.dest_label.pack(pady=5)
+        self.dest_entry = tk.Entry(root, width=60)
+        self.dest_entry.pack(pady=5)
+        self.dest_button = tk.Button(root, text="Browse", command=self.browse_dest_file)
+        self.dest_button.pack(pady=5)
+
+        # Submit button
+        self.submit_button = tk.Button(root, text="Submit", command=self.process_excel)
+        self.submit_button.pack(pady=20)
+
+        # Progress bar
+        self.progress = ttk.Progressbar(root, orient='horizontal', length=800, mode='determinate')
+        self.progress.pack(pady=10)
+
+        # Progress label
+        self.progress_label = tk.Label(root, text="")
+        self.progress_label.pack(pady=5)
+
+        # Logging area
+        self.logger_frame = Logger(root)
+        self.logger_frame.pack(pady=10)
+
+        # Configure logger
+        self.logger = logging.getLogger('ExcelProcessor')
+        self.logger.setLevel(logging.DEBUG)
+
+        # Create a file handler and set its level to DEBUG
+        file_handler = logging.FileHandler('logs.log')
+        file_handler.setLevel(logging.DEBUG)
+
+        # Create a logging format
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+        # Set the formatter for the handler
+        file_handler.setFormatter(formatter)
+
+        # Add the file handler to the logger
+        self.logger.addHandler(file_handler)
+
+        # Create a custom handler for displaying logs in the UI
+        ui_handler = TextHandler(self.logger_frame)
+        ui_handler.setLevel(logging.DEBUG)
+        ui_handler.setFormatter(formatter)
+        self.logger.addHandler(ui_handler)
+
+    def browse_source_file(self):
+        file_path = filedialog.askopenfilename(filetypes=[("Excel files", "*.xlsx *.xls")])
+        if file_path:
+            self.source_entry.delete(0, tk.END)
+            self.source_entry.insert(0, file_path)
+
+    def browse_dest_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".xlsx", filetypes=[("Excel files", "*.xlsx *.xls")])
+        if file_path:
+            self.dest_entry.delete(0, tk.END)
+            self.dest_entry.insert(0, file_path)
+
+    def process_excel(self):
+        source_file = self.source_entry.get()
+        dest_file = self.dest_entry.get()
+
+        if not source_file or not dest_file:
+            messagebox.showerror("Error", "Please select both source and destination files")
+            return
+
+        threading.Thread(target=self.process_excel_thread, args=(source_file, dest_file)).start()
+
+    def process_excel_thread(self, source_file, dest_file):
+        try:
+            self.submit_button.config(state='disabled')
+            self.progress['value'] = 0
+            self.progress_label.config(text="0% (0/0)")
+            self.logger.info("Starting Excel processing...")
+
+            # Read the source Excel file
+            df = pd.read_excel(source_file)
+            total_rows = len(df)
+            self.logger.info(f"Total rows to process: {total_rows}")
+            df.columns = ["FIRST_NAME", "LAST_NAME", "STREET", "CITY", "DIST", "ZIP"]
+
+            for index, row in df.iterrows():
+                df = process_row(row, dest_file, self.logger)
+                def show_try_again_popup():
+                    result = messagebox.askretrycancel("Error", "Updating excel could not be possible. Please close the file if you are viewing")
+                    return result
+
+                while True:
+                    try:
+                        df.to_excel(dest_file, index=False)
+                        break
+                    except:
+                        if not show_try_again_popup():
+                            continue
+                progress_percentage = (index + 1) / total_rows * 100
+                self.progress['value'] = progress_percentage
+                self.progress_label.config(text=f"{progress_percentage:.2f}% ({index + 1}/{total_rows})")
+                self.root.update_idletasks()
+
+            self.logger.info("Excel processing completed.")
+            messagebox.showinfo("Info", "Excel processing completed successfully.")
+        except Exception as e:
+            self.logger.error("Error occurred: %s", str(e))
+            self.logger.error(traceback.format_exc())
+            messagebox.showerror("Error", str(e))
+        finally:
+            self.submit_button.config(state='normal')
+            self.progress['value'] = 100
+            self.progress_label.config(text=f"100% ({total_rows}/{total_rows})")
+            self.root.quit()  # Close the program after completion
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ExcelProcessorApp(root)
+    root.mainloop()
